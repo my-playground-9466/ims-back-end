a168a3129df726067eb035583da7a2154c1d833f
Kumudi Iroshini <abeygunawardena123@gmail.com>
2023-11-22 14:29:07 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/dep11/ims/WebRootConfig.java
@@ -0,0 +1,9 @@
+package lk.ijse.dep11.ims;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class WebRootConfig {
+}
